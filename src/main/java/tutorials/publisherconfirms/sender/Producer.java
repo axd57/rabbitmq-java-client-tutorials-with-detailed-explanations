962a7dbcfb3475beadc039e6fa7bb9b9254ce982
axd57 <ahmetalper57doruk@gmail.com>
2024-03-13 21:21:31 +0300

    tutorrial 7 updated.

--- src/main/java/tutorials/publisherconfirms/sender/Producer.java
+++ src/main/java/tutorials/publisherconfirms/sender/Producer.java
@@ -14,14 +14,14 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.BooleanSupplier;
 
 public class Producer {
-    //  Publisher confirms are a RabbitMQ extension to implement reliable publishing (to make sure published messages have safely reached the broker).
-    //  When publisher confirms are ENABLED ON A CHANNEL, messages the CLIENT PUBLISHES are CONFIRMED ASYNCHRONOUSLY by THE BROKER,
+
+    //NOTES
+    //- Publisher confirms are a RabbitMQ extension TO IMPLEMENT RELIABLE PUBLISHING (to make sure published messages have safely reached the broker).
+    //- When publisher confirms are ENABLED ON A CHANNEL, messages the CLIENT PUBLISHES are CONFIRMED ASYNCHRONOUSLY by THE BROKER,
     //  meaning they have been TAKEN CARE of on the SERVER SIDE.
 
     static final int MESSAGE_COUNT = 1_000;
 
-
-    // Connecting to server.
     static Connection createConnection() throws Exception {
         ConnectionFactory cf = new ConnectionFactory();
         cf.setHost("localhost");
@@ -33,13 +33,8 @@ public class Producer {
     public static void main(String[] args) throws Exception {
         //Typical techniques are:
 
-        // publishing messages individually, waiting for the confirmation synchronously: simple, but very limited throughput.
-        // publishing messages in batch, waiting for the confirmation synchronously for a batch: simple, reasonable throughput,
-        // but hard to reason about when something goes wrong.
-        //asynchronous handling: best performance and use of resources, good control in case of error, but can be involved to
-        // implement correctly.
-
-
+        //1. publishing messages individually,
+        // waiting for the confirmation synchronously: simple, but very limited throughput.
 
         // This technique is very straightforward but also has a major drawback: it SIGNIFICANTLY SLOWS DOWN PUBLISHING,
         // as the confirmation of a message BLOCKS THE PUBLISHING OF ALL SUBSEQUENT MESSAGES. This approach is not going
@@ -49,6 +44,9 @@ public class Producer {
         publishMessagesIndividually();
 
 
+        //2. publishing messages in batch,
+        // waiting for the confirmation synchronously for a batch: simple, reasonable throughput, but hard to reason about when something goes wrong.
+
         // Waiting for a BATCH of messages to be confirmed IMPROVES THROUGHPUT DRASTICALLY OVER WAITING FOR A CONFIRM FOR
         // INDIVIDUAL MESSAGE (up to 20-30 times with a remote RabbitMQ node). one drawback is that WE DO NOT KNOW EXACTLY
         // WHAT WENT WRONG IN CASE OF FAILURE, so we may have to keep a whole batch in memory to log something meaningful
@@ -56,6 +54,8 @@ public class Producer {
 
         publishMessagesInBatch();
 
+        //3. asynchronous handling,
+        // best performance and use of resources, good control in case of error, but can be involved to implement correctly.
 
         // The broker confirms published messages asynchronously, one just needs to register a callback on the client to
         // be notified of these confirms
@@ -65,21 +65,21 @@ public class Producer {
 
     // Strategy #1
     static void publishMessagesIndividually() throws Exception {
+        //1. Connection to Server
         try (Connection connection = createConnection()) {
             Channel ch = connection.createChannel();
 
-            // Random named queue declaration.
+            //2. Random named queue declaration.
             String queue = UUID.randomUUID().toString();
             ch.queueDeclare(queue, false, false, true, null);
 
-            // Publisher confirms are a RabbitMQ extension to the AMQP 0.9.1 protocol.
-            // Not enabled by default. To enable the method calls (confirmSelect()) only ONE time for PER CHANEL.
-
-            // When sending message with Publisher confirms (using waitForConfirmsOrDie()), if this method not calls
-            // waitForConfirmsOrDie() method throws "IllegalStateException" exception.
+            //3. Publisher confirms are a RabbitMQ extension to the AMQP 0.9.1 protocol.
+            // Not enabled by default. To enable the "confirmSelect()" method calls only ONE time for PER CHANEL.
             ch.confirmSelect();
+            // When sending message with Publisher confirms (using waitForConfirmsOrDie()), if "confirmSelect()" method not calls
+            // "waitForConfirmsOrDie()" method throws "IllegalStateException" exception.
 
-            // Timeout for server's confirmation status (ack'd or nack'd)
+            //4. Timeout for server's confirmation status (ack'd or nack'd)
             long timeout = 5_000;
 
             long start = System.nanoTime();
@@ -89,16 +89,16 @@ public class Producer {
                 System.out.println(" [!] Message published to default exchange.");
                 System.out.println(" [!] Server confirmation waiting for message. (timeout " + timeout + " ms)");
                 try {
-                    // The client actually receives (FRom rabbitMQ server) confirms asynchronously.
-                    // Thinks of "waitForConfirmsOrDie" AS A SYNCHRONOUS HELPER which relies on ASYNCHRONOUS NOTIFICATIONS UNDER THE HOOD.
+                    //5. The client actually receives (From rabbitMQ server) confirms ASYNCHRONOUSLY.
+                    // Thinks of "waitForConfirmsOrDie()" AS A SYNCHRONOUS HELPER which relies on ASYNCHRONOUS NOTIFICATIONS UNDER THE HOOD.
                     // Waits until ALL MESSAGES published since the LAST CALL have been either "ack'd" or "nack'd" BY THE BROKER.
                     ch.waitForConfirmsOrDie(timeout);
 
-                // If the timeout expires a TimeoutException is thrown.
+                //5.1. If the timeout expires a "TimeoutException" is thrown.
                 } catch (TimeoutException e) {
                     System.out.println(" [!] timeout (" + timeout + " ms) expired.");
 
-                // If ANY of the MESSAGES were "nack'd", throw an IOException.
+                //5.2. If ANY of the MESSAGES were "nack'd", throw an "IOException".
                 } catch (IOException e) {
                     System.out.println(" [!] Message nack'd from server.");
                 }
@@ -107,27 +107,29 @@ public class Producer {
 
             }
             long end = System.nanoTime();
+
             System.out.format("Published %,d messages individually in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
         }
     }
 
     // Strategy #2
     static void publishMessagesInBatch() throws Exception {
+        //1. Connection to Server
         try (Connection connection = createConnection()) {
             Channel ch = connection.createChannel();
 
-            // Random named queue.
+            //2. Random named queue.
             String queue = UUID.randomUUID().toString();
             ch.queueDeclare(queue, false, false, true, null);
 
-            //publisher confirmation enables.
+            //3. publisher confirmation enables.
             ch.confirmSelect();
 
-            // bath size for divide server confirmation checks (REDUCE SYNCHRONOUS WAITING and INCREASE MESSAGE SENDING THROUGHPUT)
+            //4. Bath size for divide server confirmation checks (REDUCE SYNCHRONOUS WAITING and INCREASE MESSAGE SENDING THROUGHPUT)
             int batchSize = 100;
             int outstandingMessageCount = 0;
 
-            // Timeout for server's confirmation status (ack'd or nack'd)
+            //5. Timeout for server's confirmation status (ack'd or nack'd)
             long timeout = 5_000;
 
             long start = System.nanoTime();
@@ -141,7 +143,7 @@ public class Producer {
 
                     System.out.println(" [!] Server confirmation status checking for " + outstandingMessageCount + " messages. (timeout " + timeout + " ms)");
                     ch.waitForConfirmsOrDie(timeout);
-                    // may be IOException (throws if any message in batch, server returns for "nack'd") or
+                    // may be IOException (throws if ANY message in batch, server returns for "nack'd") or
                     // TimeoutException (throws if server can not return all message's (in batch) confirmation results in specified timeout time.)
                     // ...
 
@@ -161,26 +163,29 @@ public class Producer {
                 System.out.println(" [!] Server confirmed " + outstandingMessageCount + " messages. (timeout " + timeout + " ms)");
             }
             long end = System.nanoTime();
+
             System.out.format("Published %,d messages in batch in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
         }
     }
 
     // Strategy #3
     static void handlePublishConfirmsAsynchronously() throws Exception {
+        //1. Connection to Server
         try (Connection connection = createConnection()) {
             Channel ch = connection.createChannel();
 
-            // Random named queue declaration.
+            //2. Random named queue declaration.
             String queue = UUID.randomUUID().toString();
             ch.queueDeclare(queue, false, false, true, null);
 
-            //  //publisher confirmation enables.
+            //3. publisher confirmation enables.
             ch.confirmSelect();
 
-            // Using for concurrent support and CORRELATE the PUBLISHING SEQUENCE NUMBER WITH A MESSAGE.
+            //4. Using for concurrent support and CORRELATE the PUBLISHING SEQUENCE NUMBER WITH A MESSAGE.
             ConcurrentNavigableMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
 
 
+            //5. Callback method parameters,
 
             // sequenceNumber: Server's (RabbitMQ node) confirmation returned MESSAGE's or LAST MESSAGE's (between  multiple messages,
             // if "multiple" parameters is "true") sequence number.
@@ -189,7 +194,8 @@ public class Producer {
             // with a lower or equal sequence number are confirmed/nack-ed.
 
 
-            //ACK
+            //5.1. ACK
+
             // Triggers when server's confirm message.
             ConfirmCallback cleanOutstandingConfirms = (sequenceNumber, multiple) -> {
                 if (multiple) {
@@ -205,7 +211,8 @@ public class Producer {
                 }
             };
 
-            //NACK
+            //5.2. NACK
+
             // Trigger when messages lost by the broker (confirmation result nack-ed).
             // Note: the lost messages (nack-ed) COULD STILL have been DELIVERED TO CONSUMERS, but the BROKER CANNOT GUARANTEE this.
             ConfirmCallback nackMessages = (sequenceNumber, multiple) -> {
@@ -218,33 +225,37 @@ public class Producer {
                 System.out.println(" [!] Nack-ed message cleared from \"outstandingConfirms\" map.");
             };
 
-            // For listening server's message confirmations asynchronously.
+            //5.3. For listening server's message confirmations asynchronously.
             // First parameter callback triggers when message(s) ack-ed from server.
             // Second parameter callback triggers when message(s) nack-ed from server.
             ch.addConfirmListener(cleanOutstandingConfirms, nackMessages);
 
             long timeout = 60;
 
+
             long start = System.nanoTime();
             for (int i = 0; i < MESSAGE_COUNT; i++) {
                 String body = String.valueOf(i);
 
-                // In confirm mode published message sequence no. (example: 1,2,3,...)
+                //6. In confirm mode published message sequence no. (example: 1,2,3,...)
                 long sequenceNumber = ch.getNextPublishSeqNo();
 
-                // For tracking, the publishing sequence number before publishing a message.
+                //6.1. For tracking, the publishing sequence number before publishing a message.
                 outstandingConfirms.put(sequenceNumber, body);
                 System.out.println(" [!] Message added to \"outstandingConfirms\" map, with " + sequenceNumber + " sequence number.");
 
+                //6.2. Message publishing to AMQP Default exchange.
                 ch.basicPublish("", queue, null, body.getBytes());
                 System.out.println(" [!] Message sent to default exchange.");
             }
 
+            //7. Confirmation timeout control.
             if (!waitUntil(Duration.ofSeconds(timeout), () -> outstandingConfirms.isEmpty())) {
                 throw new IllegalStateException("All messages could not be confirmed in " + timeout + " seconds");
             }
 
             long end = System.nanoTime();
+
             System.out.format("Published %,d messages and handled confirms asynchronously in %,d ms%n", MESSAGE_COUNT, Duration.ofNanos(end - start).toMillis());
         }
     }
